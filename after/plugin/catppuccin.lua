e9132f1b0667366c08ff743478d305e3ebb98953
Jellu <105118442+Debilesque@users.noreply.github.com>
2022-12-26 12:20:35 -0600

    catppuccin config

---
+++ after/plugin/catppuccin.lua
@@ -0,0 +1,15 @@
+require('catppuccin').setup {
+    color_overrides = {
+        latte = {
+            base     = "#f5f3f0";
+            text     = "#18181A";
+
+            surface2 = "#38393d";
+            surface1 = "#9699a3";
+            overlay0 = "#acb0be";
+
+            lavender = "#5b6cca";
+        },
+    },
+    transparent_background = false;
+}
