ba65e9f56a36af5842093f85b6e9de9794ee9039
Jellu <105118442+Debilesque@users.noreply.github.com>
2022-12-26 12:01:02 -0600

    mayor update 26/12

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,223 +1,215 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+------------------------------------------------
+--  ____            _
+-- |  _ \ __ _  ___| | ____ _  __ _  ___  ___ 
+-- | |_) / _` |/ __| |/ / _` |/ _` |/ _ \/ __|
+-- |  __/ (_| | (__|   < (_| | (_| |  __/\__ \
+-- |_|   \__,_|\___|_|\_\__,_|\__, |\___||___/
+--                            |___/
+------------------------------------------------
+
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+    PACKER_BOOTSTRAP = fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    }
+    print "Installing packer; close and reopen Neovim..."
     vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
 end
 
-local packer_bootstrap = ensure_packer()
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+    augroup packer_user_config
+        autocmd!
+        autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    return
+end
+
+-- Have packer use a popup window
+packer.init {
+    display = {
+        open_fn = function()
+        return require("packer.util").float { border = "rounded" }
+    end,
+    },
+}
 
 return require('packer').startup(
     function(use)
 
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+    -- ====>Packer
+        use 'wbthomason/packer.nvim'
 
     -- ===> ColorSchemes
-    use 'ellisonleao/gruvbox.nvim'
-    use 'folke/tokyonight.nvim'
-    use 'savq/melange'
-    
-    use {
-        'catppuccin/nvim',
-        as = 'catppuccin'
-    }
-    
-    use {
-        "mcchrish/zenbones.nvim",
-        requires = "rktjmp/lush.nvim"
-    }
-    
-    use {
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        tag = 'v1.*',
-        config = function()
-        end
-    }
-    
+        use 'ellisonleao/gruvbox.nvim'
+        use 'folke/tokyonight.nvim'
+        use 'savq/melange'
+
+        use {
+            'catppuccin/nvim',
+            as = 'catppuccin'
+        }
+
+        use {
+            "mcchrish/zenbones.nvim",
+            requires = "rktjmp/lush.nvim"
+        }
+
+        use {
+            'rose-pine/neovim',
+            as = 'rose-pine',
+            tag = 'v1.*',
+            config = function()
+            end
+        }
+
     -- ===> Treesitter
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            ts_update()
-        end,
-    }
+        use {
+            'nvim-treesitter/nvim-treesitter',
+            run = function()
+                local ts_update = require('nvim-treesitter.install').update({
+                    with_sync = true
+                })
+                ts_update()
+            end,
+        }
 
     -- ===> LSP
 
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use {
-        'hrsh7th/cmp-path',
-        require('cmp').setup {
-            sources = { name = 'path'}
-        }
-    }
-    
-    -- ===> Completion
-    use {
-        'windwp/nvim-autopairs',
-        config = function()
-            require('nvim-autopairs').setup {
+        use 'neovim/nvim-lspconfig'
+        use 'williamboman/mason.nvim'
+        use 'williamboman/mason-lspconfig.nvim'
+        use 'VonHeikemen/lsp-zero.nvim'
+        use 'j-hui/fidget.nvim'
+
+        use 'folke/neodev.nvim'
+
+    -- ==== Completion Engines
+        use 'hrsh7th/nvim-cmp'
+        use 'hrsh7th/cmp-path'
+        use 'hrsh7th/cmp-nvim-lsp'
+        use 'hrsh7th/cmp-buffer'
+        use {
+            'hrsh7th/cmp-path',
+            require('cmp').setup {
+                sources = { name = 'path'}
             }
-        end
-    }
-
-    use 'j-hui/fidget.nvim'
-    use 'folke/neodev.nvim'
-
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-
-        requires = {
-
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-        
-            -- Autocompletion
-            {'hrsh7th/cmp-buffer'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-        
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
         }
-    }
 
-    -- ===> Navigation
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = {
-            'nvim-lua/plenary.nvim'
+    -- ==== Snippets Engines
+        use 'L3MON4D3/LuaSnip'
+        use 'rafamadriz/friendly-snippets'
+
+        use 'saadparwaiz1/cmp_luasnip'
+        use 'hrsh7th/cmp-nvim-lsp'
+        use 'hrsh7th/cmp-nvim-lua'
+
+    -- ===> Autopair & Surround
+        use {
+            'windwp/nvim-autopairs',
+            config = function()
+                require('nvim-autopairs').setup {
+                }
+            end
         }
-    }
 
-    use {'theprimeagen/harpoon'}
-
-    use {'mbbill/undotree'}
-
-    -- ===> Beautify
-    use 'maksimr/vim-jsbeautify'
-    
-    -- ===> Nerdtree
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = 'kyazdani42/nvim-web-devicons',
-        config = function()
-            require('nvim-tree').setup {
-            }
-        end
-    }
-    
-    -- ===> StatusLine
-    
-    -- ===> Colorizers
-    use {
-    'ziontee113/color-picker.nvim',
-    config = function()
-        require("color-picker").setup{
-        }
-    end
-    }
+        use 'tpope/vim-surround'
 
     -- ===> Commenting
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup {
-            }
-        end,
-    }
-    
-    -- ===> Markdown Live Preview
-    -- install without yarn or npm
-    use {
-        'iamcco/markdown-preview.nvim',
-        run = 'cd app && npm install',
-        setup = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-        end,
-        ft = { "markdown" }
-    }
-    
-    -- ===> HTML/CSS/JS live preview (default browser)
-    use {
-        'turbio/bracey.vim',
-        run = 'npm instal --prefix server'
-    }
-    
-    -- ===> Latex
-    use {
-        'vim-latex/vim-latex'
-    }
+        use {
+            'numToStr/Comment.nvim',
+            config = function()
+                require('Comment').setup {
+                }
+            end,
+        }
 
-    -- ===> Clipboard
-    -- use 'christoomey/vim-system-copy'
-    
-    -- ===> surround
-    use 'tpope/vim-surround'
-    
-    -- ===> Fuzzy Search
-    -- depends on the fzf package
-    use 'junegunn/fzf.vim'
-    
-    -- ===> Alignment
-    use 'godlygeek/tabular'
-    
-    -- ===> Previous Files
-    use 'yegappan/mru'
-    
+    -- ====> Saving Folds & Cursor Position
+        use 'vim-scripts/restore_view.vim'
+
+    -- ====> Tabulation
+        use 'godlygeek/tabular'
+        use 'Yggdroot/indentLine'
+        use 'tpope/vim-sleuth'
+        use 'lukas-reineke/indent-blankline.nvim'
+
+    -- ===> Navigation
+        use {
+            'nvim-telescope/telescope.nvim', tag = '0.1.0',
+            requires = {
+                'nvim-lua/plenary.nvim'
+            }
+        }
+
+        use 'theprimeagen/harpoon'
+        use 'mbbill/undotree'
+        use 'junegunn/fzf.vim'
+
+    -- ===> Nerdtree
+        use {
+            'kyazdani42/nvim-tree.lua',
+            requires = 'kyazdani42/nvim-web-devicons',
+            config = function()
+                require('nvim-tree').setup {
+                }
+            end
+        }
+
+    -- ===> StatusLine
+        use 'nvim-lualine/lualine.nvim'
+
+    -- ===> Colorizers
+        use {
+        'ziontee113/color-picker.nvim',
+        config = function()
+            require("color-picker").setup{
+            }
+        end
+        }
+
     -- ===> Start Screen
     use 'mhinz/vim-startify'
-    
+    use 'yegappan/mru'
+
     -- ===> Floterm
     use 'voldikss/vim-floaterm'
-    
-    -- ===> indentline
-    use 'Yggdroot/indentLine' 
-    
+
     -- ===> WhichKey
     use {
         'folke/which-key.nvim',
         config = function()
             require('which-key').setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
             }
         end
-    }
-
-    -- ===> Jupyter Integration
-    use {
-        'jupyter-vim/jupyter-vim',
-        as = 'jupyter'
     }
 
     -- ===> Games
     use 'ThePrimeagen/vim-be-good'
 
-      use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-  use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-  use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+    -- -- ===> Latex
+    -- use {'vim-latex/vim-latex'}
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+    -- -- ===> Clipboard
+    --     use 'christoomey/vim-system-copy'
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 
 end)
