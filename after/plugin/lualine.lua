fabeea8ea646ff40a38d7701b2f7ee8c68243915
Jellu <105118442+Debilesque@users.noreply.github.com>
2022-12-29 18:32:31 -0600

    added lualine config

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -4,13 +4,27 @@ require('lualine').setup {
     theme = 'onelight',
     component_separators = { left = '|', right = '|'},
     section_separators = { left = '', right = ''},
+    -- always_divide_middle = false;
+    -- color = {fg = '#ffaa88', bg = 'grey'},
     },
 
     sections = {
-        lualine_a = {'mode'},
+        lualine_a = {
+            'mode',
+        },
         lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
-        lualine_x = {'filetype'},
+        lualine_c = {
+            {
+                'filename',
+                -- color = {fg = '#ffaa88', bg = 'grey'},
+            }
+        },
+        lualine_x = {
+            {
+                'filetype',
+                -- color = {fg = '#ffaa88', bg = 'grey'},
+            }
+        },
         lualine_y = {'progress'},
         lualine_z = {'location'}
     },
