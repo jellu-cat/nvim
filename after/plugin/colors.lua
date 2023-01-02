26a7a6c4dddaeff903a988762b57f40744246170
Jellu <105118442+Debilesque@users.noreply.github.com>
2023-01-02 12:18:53 -0600

    restored non-transparent background

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,8 +2,8 @@ function Color()
     color = color or 'catppuccin'
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none'})
-    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none'})
+    -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none'})
+    -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none'})
 end
 
 Color()
